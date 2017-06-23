40c117f89e5a4e7ffdd3bc9ec94fee74859e5c3e
MIN <a0928303863@gmail.com>
2017-06-23 16:48:12 +0800

    成功登入

--- app/src/main/java/info/androidhive/loginandregistration/app/AppConfig.java
+++ app/src/main/java/info/androidhive/loginandregistration/app/AppConfig.java
@@ -2,8 +2,8 @@ package info.androidhive.loginandregistration.app;
 
 public class AppConfig {
 	// Server user login url
-	public static String URL_LOGIN = "http://10.0.2.2/android_login_api/login.php";
+	public static String URL_LOGIN = "http://210.70.179.27/android_login_api/login.php";
 
 	// Server user register url
-	public static String URL_REGISTER = "http://10.0.2.2/android_login_api/register.php";
+	public static String URL_REGISTER = "http://210.70.179.27/android_login_api/register.php";
 }
